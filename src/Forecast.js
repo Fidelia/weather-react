d2819be9457d33454680cedd19350cfa10a2788b
Fidelia <fidelia_okandze@hotmail.co.uk>
2021-07-30 20:20:07 +0100

    loaded conditional rendering

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,29 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./Forecast.css";
 import axios from "axios";
 
 export default function Forecast(props) {
-  function displayForecast(coordinates) {}
-  let lon = props.coordinates.lon;
-  let lat = props.coordinates.lat;
-  let apiKey = "42d452330bfdae27782fbf2b6fe4218a";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
 
-  axios.get(apiUrl).then(displayForecast);
+  function displayForecast(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
 
-  return (
-    <div className="Forecast">
-      <div className="row">
-        <div className="col">
-          <div className="Forecast-day">Thu</div>
-          <WeatherIcon code="01d" size={36} />
-          <div className="Forecast-temperatures">
-            <span className="Forecast-max">20</span>
-            <span className="Forecast-min">10</span>
+  if (loaded) {
+    return (
+      <div className="Forecast">
+        <div className="row">
+          <div className="col">
+            <div className="Forecast-day">Thu</div>
+            <WeatherIcon code="01d" size={36} />
+            <div className="Forecast-temperatures">
+              <span className="Forecast-max">{forecast[0].temp.max}</span>
+              <span className="Forecast-min">{forecast[0].temp.min}</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "42d452330bfdae27782fbf2b6fe4218a";
+    let longitude = props.lon;
+    let latitude = props.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(displayForecast);
+    return null;
+  }
 }
