1c9410c5110c4820baf93dcdc8b8cbefb649686d
Fidelia <fidelia_okandze@hotmail.co.uk>
2021-08-07 09:34:50 +0100

    import forecast removed fro app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
 import WeatherSearch from "./WeatherSearch";
-import Forecast from "./Forecast";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <WeatherSearch defaultCity="London" />
-        <Forecast />
       </div>
       <footer>
         <a
